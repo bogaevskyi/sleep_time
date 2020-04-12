536315d8229ed34586d293b9b4c82143ad19d9bf
Andrew Bogaevskyi <andrew.bogaevskyi@gmail.com>
2020-04-12 13:09:00 +0300

    Initial project setup.

---
+++ SleepTime/SleepTime/SupportingFiles/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  SleepTime
+//
+//  Created by Andrew Bogaevskyi on 12.04.2020.
+//  Copyright © 2020 Andrew Bogaevskyi. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = HomeViewController()
+        window.makeKeyAndVisible()
+        
+        self.window = window
+    }
+}
+
